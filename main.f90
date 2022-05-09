8a816a0a5cbe2d4b55e810e5dec928c75a93c5cd
fraa182 <102958271+fraa182@users.noreply.github.com>
2022-05-09 12:07:58 +0200

    Update main.f90

--- main.f90
+++ main.f90
@@ -24,7 +24,7 @@ program CFD
     !call WDKs_tk(0,-3, "grid", x,y,nc,mc,nc,mc,xg,yg,a,u,v,s)! file da aprire in visit, add - mesh - draw
 
     call init
-    call WDKs_tk(0,-3, "grid", x,y,nc,mc,nc,mc,xg,yg,a,u,v,s)! file da aprire in visit, add - mesh - draw
+    call WDKs_tk(0,1, "grid", x,y,nc,mc,nc,mc,xg,yg,a,u,v,s)! file da aprire in visit, add - mesh - draw
 
     !time=0.
 
